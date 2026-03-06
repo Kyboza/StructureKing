36f64b5453f51a398af03cf78b92a69f149199b9
Kyboza <johanclifford746@gmail.com>
2026-03-06 14:17:41 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,24 @@
+// src/frontend/vite.config.ts
 import path from "path"
 
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig, loadEnv } from "vite"
 
-
-
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
   return {
-    plugins: [tailwindcss(), react()],
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
-       "@": path.resolve(__dirname, "src")
+        "@": path.resolve(__dirname), // @ -> src/frontend
       },
     },
     base: env.VITE_BASE_PATH || "/",
+    build: {
+      outDir: "dist", // bygg till dist i frontend-mappen
+      emptyOutDir: true,
+    },
   }
 })
