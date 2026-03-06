cf050cea30da8b50d976ec22994d97f663eafbc4
Kyboza <johanclifford746@gmail.com>
2026-03-06 14:36:18 +0100

    test22

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,24 @@
-// vite.config.ts
-import path from "path";
+// src/frontend/vite.config.ts
+import path from "path"
 
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig} from "vite";
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig, loadEnv } from "vite"
 
-export default defineConfig(() => {
-  // const env = loadEnv(mode, process.cwd());
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
 
   return {
-    root: path.resolve(__dirname), // <-- root är projektets root
-    base: "/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "src/frontend"),
+        "@": path.resolve(__dirname), // @ -> src/frontend
       },
     },
+    base: env.VITE_BASE_PATH || "/",
     build: {
-      outDir: path.resolve(__dirname, "dist/frontend"),
+      outDir: "dist", // bygg till dist i frontend-mappen
       emptyOutDir: true,
     },
-  };
-});
+  }
+})
